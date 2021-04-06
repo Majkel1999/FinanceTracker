c496751afa69a33b355250adfc7c6eba6a8ce65f
Michał Kaleta <mkaleta1999@gmail.com>
2021-04-06 20:24:20 +0200

    Poprawione komentarze

--- FinanceTracker/FinancialData.cs
+++ FinanceTracker/FinancialData.cs
@@ -1,19 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
 namespace FinanceTracker
 {
+    /// <summary>
+    ///    Klasa statyczna odpowiedzialna za otrzymywanie i parsowanie danych z serwera
+    /// </summary>
     public static class FinancialData
     {
+        /// <summary>
+        /// Metoda pobiera dane z serwera i deserializuje je do obiektów.
+        /// Pobrane dane są deserializowane z JSON.
+        /// </summary>
+        /// <param name="stockIndex">Symbol dla ktorego pobrac cene</param>
+        /// <returns>Tablica StockPrice[] o jednym elemencie</returns>
         public async static Task<StockPrice[]> GetStockIndexPrice(string stockIndex)
         {
             string apiTask = await ApiRequest.GetStockPrice(stockIndex);
             return JsonConvert.DeserializeObject<StockPrice[]>(apiTask);
         }
+        /// <summary>
+        /// Metoda pobiera wszystkie indeksy dostepne na gieldzie.
+        /// Usuwane sa wszystkie indeksy, ktorych nazwa zawiera znak '.', cena jest rowna 0 
+        /// lub nazwa jest pusta, lub nie ustawiona.
+        /// </summary>
+        /// <returns>Lista StockIndex na ktorej sa wszystkie dostepne symbole</returns>
         public async static Task<List<StockIndex>> GetAllStockIndexes()
         {
             string apiTask = await ApiRequest.GetStockIndexes();
@@ -24,6 +38,11 @@ namespace FinanceTracker
             indexList.RemoveAll(s => s.name == "");
             return indexList;
         }
+        /// <summary>
+        /// Metoda pobiera pełne dane historyczne i deserializuje je do obiektu.
+        /// </summary>
+        /// <param name="stockIndex">Symbol dla ktorego pobrac dane</param>
+        /// <returns>Lista pelnych danych historycznych</returns>
         public async static Task<List<HistoricalIndexData>> GetHistoricalIndexData(string stockIndex)
         {
             string result = await ApiRequest.GetHistoricalData(stockIndex);
@@ -42,6 +61,12 @@ namespace FinanceTracker
             }
             return historicalIndexData;
         }
+        /// <summary>
+        /// Metoda pobiera dane historyczne i deserializuje je do obiektu.
+        /// </summary>
+        /// <param name="stockIndex">Symbol dla ktorego pobrac dane</param>
+        /// <param name="startDate">Data od ktorej pobrac dane</param>
+        /// <returns>Lista danych historycznych od zadanej daty</returns>
         public async static Task<List<HistoricalIndexData>> GetHistoricalIndexData(string stockIndex,DateTime startDate)
         {
             string result = await ApiRequest.GetHistoricalData(stockIndex,startDate);
